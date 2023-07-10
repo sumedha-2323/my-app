17b61bc0b17e17b6d4b59d31e338455dee61aed2
sumedha-2323 <sumedhajammula@gmail.com>
2023-07-10 20:12:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+ const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <section className="movie-container">
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  <Movie />
+  </section> 
   );
-}
+};
+const Movie = () => {
+  return ( 
+  <article className="each-movie">
+    <ImageComponent />
+    <Title />
+  </article>
+  );
+};
 
+
+const ImageComponent = () => {
+  return(
+     <img src="https://images.lifestyleasia.com/wp-content/uploads/sites/7/2020/11/19143826/Mismatched-01_Netflix-min-1600x900.jpg?tr=w-1600" 
+     alt="netflix image" height="150px" weight="150px" />
+  );
+};
+
+
+const Title = () => {
+
+  //  const styles =  {
+  //   heading: {
+  //     color: "red",
+  //     fontStyle: "italic",
+  //   },
+  //  };
+  // return <h3 style={{color:"red", fontStyle:"italic"}}> Mismatched </h3>
+  return (
+  // <h3 style={styles.heading}> Mismatched</h3>
+     <h3> Mismatched </h3>
+  );
+};
 export default App;
+
